166bca1958d588e8475b254c13ed25dc0aeb215a
susindhar21 <susindhar02@gmail.com>
2023-01-08 10:55:53 +0530

    Change to markdown

--- hello.py
+++ hello.py
@@ -49,23 +49,11 @@ st.sidebar.success("Select a demo above.")
 
 st.markdown(
     """
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
-    ### Want to learn more?
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-"""
+    கமாவால் பிரிக்கப்பட்ட சொற்களை உள்ளிடவும்.
+    """
 )
 
-inp_text = st.text_input('Enter some text','')
+inp_text = st.text_input('','')
 
 if(inp_text != ''):
 
